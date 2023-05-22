3a3a9b26fe3b40db2bc17969725481f975b2b651
TACOWASA_1231 <98175856+TeamSquidTempura@users.noreply.github.com>
2023-05-22 15:03:10 +0900

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = "ENTER-THE-API-KEY-HERE"
+for i in range(999):
+    prompt = input("Enter the prompt:")
+    print("loading...")
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            },
+        ],
+    )
+    print("-----CHAT-GPT-3.5-TURBO-----")
+    print(res["choices"][0]["message"]["content"])
+    print("----------------------------")
